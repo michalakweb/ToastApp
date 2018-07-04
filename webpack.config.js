d3eb10f27597b46c682deb2c040048b468d9b75e
Mateusz Michalak <michalakweb@gmail.com>
2018-07-04 13:02:12 +0700

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+
+module.exports = {
+	entry: {
+		app: './src/main.js'
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+	rules: [
+	{
+		test: /\.(png|svg|jpg|gif)$/,
+		use: {
+			loader: 'file-loader'
+		}
+	},
+	{
+		test: /\.js$/,
+		exclude: /(node_modules|bower_components)/,
+		use: {
+			loader: 'babel-loader',
+		}
+	},
+	{
+	test: /\.scss$/,
+	use: [
+		{
+		loader: 'style-loader'
+		},
+		{
+		loader: 'css-loader'
+		},
+		{
+			loader: 'sass-loader'
+		}
+	]
+	}
+]
+},
+	target: 'web',
+	node: {
+		fs: 'empty',
+		net: 'empty',
+		tls: 'empty'
+	}
+}
